b7688096d1c434fe4d220339348eb05a8de83bb0
Aidy <aidyszh@gmail.com>
2018-01-04 15:05:29 +0800

    successfully tested with week 4 assignment data

--- week_4_building_deep_neural_network.py
+++ week_4_building_deep_neural_network.py
@@ -1,4 +1,19 @@
 
+"""
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+import matplotlib.pyplot as plt
+"""
+%matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
 #################### Helpers ######################
 
 def relu_func(x):    
@@ -37,7 +52,7 @@ def init(layer_dims):
     Returns:
     parameters: dictionary contains W and b of each layer (index examples: "W1", "b3")
     """ 
-    np.random.seed(2)
+    np.random.seed(1)
     parameters = {}
     L = len(layer_dims)
 
@@ -88,11 +103,19 @@ def linear_backward(dA, A_prev, Z, W, activation_func):
 
     return dZ, dW, db, dA_prev
 
-def deep_neural_network(X, Y, layer_dims = [X.shape[0], 4, 1], layer_gs = ['', 'tanh', 'sigmoid'], iterations = 10000, learning_rate = 1.2, print_cost = True):
+def deep_neural_network(X, Y, layer_dims, layer_gs, iterations = 10000, learning_rate = 1.2, print_cost = True):
+    print("X.shape = " + str(X.shape))
+    print("Y.shape = " + str(Y.shape))
+    print("layer_dims = " + str(layer_dims))
+    print("layer_gs = " + str(layer_gs))
+    print("iterations = " + str(iterations))
+    print("learning_rate = " + str(learning_rate))
 
-    parameters = init(layer_dims)
+    # *NOTE*: init(layer_dims) in notebook may get diff result comparing with running initialize_parameters_deep() on server, even with same seed.
+    parameters = initialize_parameters_deep(layer_dims)
     L = len(layer_dims)
-
+    
+    costs = []
     for iter in range(iterations):
         caches = {} # contains Z and A of each layer, caches['A0'] = X
         caches['A0'] = X
@@ -105,13 +128,31 @@ def deep_neural_network(X, Y, layer_dims = [X.shape[0], 4, 1], layer_gs = ['', '
             A_prev = A
 
         J = cost(A, Y)
-        if print_cost and iter % 1000 == 0:
+        if print_cost and iter % 100 == 0:
             print("cost at iteration %i : %f" %(iter, J))
+            costs.append(J)
 
+        
         dA = -1 * (Y / A) + (1 - Y) / (1 - A)
         for l in range(L - 1, 0, -1):
             dZ, dW, db, dA_prev = linear_backward(dA, caches['A' + str(l-1)], caches['Z' + str(l)], parameters['W' + str(l)], layer_gs[l])
             parameters['W' + str(l)] = parameters['W' + str(l)] - learning_rate * dW
             parameters['b' + str(l)] = parameters['b' + str(l)] - learning_rate * db
             dA = dA_prev
-    print(parameters)
+    # print(parameters)
+    # plot the cost
+
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    return parameters
+    
+
+"""
+# week 4 assignment test
+parameters = deep_neural_network(train_x, train_y, layer_dims = [12288, 20, 7, 5, 1], layer_gs = ['', 'relu', 'relu', 'relu', 'sigmoid'], iterations = 1000, learning_rate = 0.0075, print_cost = True)
+predictions_train = predict(train_x, train_y, parameters)
+predictions_test = predict(test_x, test_y, parameters)
+"""
